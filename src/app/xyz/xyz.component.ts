a1a40b473c7ea3d957a324c762ed7975d487d9c8
GabrielFSouza <gabrielferreira717@outlook.com>
2024-07-09 12:20:47 -0300

    #001 - Push Project

---
+++ src/app/xyz/xyz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xyz',
+  standalone: true,
+  imports: [],
+  templateUrl: './xyz.component.html',
+  styleUrl: './xyz.component.scss'
+})
+export class XyzComponent {
+
+}
